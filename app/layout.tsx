87143de3f4d1613f9f8c5d861e32778d28d8ea37
Alice do Valle <alicedovalle.r@gmail.com>
2025-07-19 19:34:52 -0300

    Altera layout padrão

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="max-w-3xl mx-auto px-4">
+          <Navbar/>
+          <div className="mt-8">
+            {children}
+          </div>
+        </div>
       </body>
     </html>
   );
