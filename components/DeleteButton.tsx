98df556176f91004feae46e37b73c9188a48a1c6
Alice do Valle <alicedovalle.r@gmail.com>
2025-07-19 19:34:03 -0300

    Cria componente de "excluir tarefa"

---
+++ components/DeleteButton.tsx
@@ -0,0 +1,9 @@
+import { VscTrash } from "react-icons/vsc"
+
+export default function DeleteButton() {
+    return ( 
+    <button className="text-[#ff66b3]">
+        <VscTrash size={28}/>
+    </button>
+    )
+}
